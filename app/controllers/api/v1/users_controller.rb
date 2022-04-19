7be5c697a187281c5b54d1fa7012cf5a38d020d4
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2022-04-19 20:05:58 +0300

    user is able to signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :authentication, except: %i[signup login]
+
+  def signup
+    user = User.create(user_params)
+    if user.save
+      token = encode_user_data({ user_data: user.id })
+      render json: {
+        message: 'user created successfully',
+        token: token,
+        code: 201
+      }
+    else
+      render json: user.errors.messages
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
+  end
 end
